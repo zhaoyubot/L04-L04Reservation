67daa395be7a42442ecfd3d56918cc04dd42cac6
zhaoyubot <20011119@myrp.edu.sg>
2022-05-13 00:00:17 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20011119/l04_reservation/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id20011119.l04_reservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
